be6f50fb1ed0b4839535e9c1669e21f2d5b7bc45
KochenkovSA <bouviparty@gmail.com>
2025-08-31 23:57:25 +0300

    Step 3: - created decryption method; - updated constants

--- src/main/java/com/javarush/kochenkov/Constants.java
+++ src/main/java/com/javarush/kochenkov/Constants.java
@@ -7,4 +7,9 @@ public interface Constants {
             "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
             "0123456789" +
             " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n\r\t";
+
+    String INPUT_FILE_PATH = "Enter the path to the file: ";
+    String OUTPUT_FILE_PATH = "Specify the path to the save location: ";
+    String CAESAR_SHIFT = "Specify the letter shift: ";
+
 }
--- src/main/java/com/javarush/kochenkov/MainMenu.java
+++ src/main/java/com/javarush/kochenkov/MainMenu.java
@@ -36,32 +36,58 @@ public class MainMenu {
 
     private void encrypt() {
         try {
-            Scanner localScanner = new Scanner(System.in); // !!!Пришлось сделать локальный сканер из-за ошибки возникающей при указании путей IO и значения сдвига!!!
+            Scanner encryptScanner = new Scanner(System.in);
             SymbolMapper symbolMapper = new SymbolMapper();
             Encrypt cipher = new Encrypt(symbolMapper);
-            System.out.println("***** Caesar's Encryptor *****");
-            System.out.print("Enter the path to the file: ");
-            String inputFile = localScanner.nextLine();
+            System.out.println("***** Caesar's Encryption *****");
+            System.out.print(Constants.INPUT_FILE_PATH);
+            String inputFile = encryptScanner.nextLine();
 
-            System.out.print("Specify the path to the save location: ");
-            String outputFile = localScanner.nextLine();
+            System.out.print(Constants.OUTPUT_FILE_PATH);
+            String outputFile = encryptScanner.nextLine();
 
-            System.out.print("Specify the letter shift: ");
-            int shift = Integer.parseInt(localScanner.nextLine());
-
+            System.out.print(Constants.CAESAR_SHIFT);
+            int shift = encryptScanner.nextInt();
 
             cipher.encryptFile(inputFile, outputFile, shift);
+            System.out.printf("Your file is encrypted! Shift : %d%n", shift);
             quit();
 
         } catch (FileNotFoundException e) {
             System.err.printf("File not found: %s%n", e.getMessage());
         } catch (IOException e) {
             System.err.printf("IO error during encryption: %s%n", e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid shift value!");
         }
     }
 
     private void decrypt() {
+        try {
+            Scanner decryptScanner = new Scanner(System.in);
+            SymbolMapper symbolMapper = new SymbolMapper();
+            Encrypt cipher = new Encrypt(symbolMapper);
+            System.out.println("***** Caesar's Decryption *****");
+            System.out.print(Constants.INPUT_FILE_PATH);
+            String inputFile = decryptScanner.nextLine();
 
+            System.out.print(Constants.OUTPUT_FILE_PATH);
+            String outputFile = decryptScanner.nextLine();
+
+            System.out.print(Constants.CAESAR_SHIFT);
+            int shift = decryptScanner.nextInt() * -1;
+
+            cipher.encryptFile(inputFile, outputFile, shift);
+            System.out.printf("Your file is decrypted! Shift : %d%n", shift);
+            quit();
+
+        } catch (FileNotFoundException e) {
+            System.err.printf("File not found: %s%n", e.getMessage());
+        } catch (IOException e) {
+            System.err.printf("IO error during decryption: %s%n", e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Invalid shift value!");
+        }
     }
 
     private void quit() {
